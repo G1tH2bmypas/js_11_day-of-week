ab03d95c1ad37d2727b855b5a03521725cbf079c
Tekyto <Tekyto2014@gmail.com>
2022-11-17 21:22:01 +0200

    js_11_day-of-week+

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 function dayOfWeek(day) {
 	switch (true) {
-		case day === 1 : return 'sunday';
-		case day === 2 : return 'monday';
-		case day === 3 : return 'tuesday'
-		case day === 4 : return 'wednesday';
-		case day === 5 : return 'thursday';
-		case day === 6 : return 'friday';
-		case day === 7 : return 'saturday';
-		default : return '';
+		case day === 1: return 'sunday';
+		case day === 2: return 'monday';
+		case day === 3: return 'tuesday';
+		case day === 4: return 'wednesday';
+		case day === 5: return 'thursday';
+		case day === 6: return 'friday';
+		case day === 7: return 'saturday';
+		default: return '';
 
 	}
 }
